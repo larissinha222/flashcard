d189e50c9787bab6d5fa0f3315691822e72732f5
Lari 🍓 <00001106130716sp@al.educacao.sp.gov.br>
2024-11-27 10:53:12 -0300

    adicionando arquivos

---
+++ ultimo projeto/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Moda',
+    'O que é balone?',
+    'Tipo de roupa que parece um balão'
+)
+
+criaCartao(
+    'Maquiagem',
+    'O que é primer?',
+    'Produto para preparar a pele que vai receber maquiagem'
+)
+
+criaCartao(
+    'Esmaltes',
+    'De qual marca é o esmalte merengue suíço?',
+    'Impala'
+)
+
+criaCartao(
+    'Cantoras',
+    'De quem é o hit Please Please Please?',
+    'Sabrina Carpinter'
+)
